3f53e80314b9035ff4eb6dcdb21cec75bc0e4436
banky <bankiadebajo1@gmail.com>
2022-02-15 20:17:00 -0500

    Add sole and anke

---
+++ sole-and-ankle/src/components/SuperHeader/SuperHeader.js
@@ -0,0 +1,53 @@
+import React from "react";
+import styled from "styled-components";
+
+import { COLORS } from "../../constants";
+
+import SearchInput from "../SearchInput";
+import UnstyledButton from "../UnstyledButton";
+import Icon from "../Icon";
+
+const SuperHeader = () => {
+  return (
+    <Wrapper>
+      <MarketingMessage>
+        Free shipping on domestic orders over $75!
+      </MarketingMessage>
+      <SearchInput />
+      <HelpLink href="/help">Help</HelpLink>
+      <UnstyledButton>
+        <Icon id="shopping-bag" strokeWidth={1} />
+      </UnstyledButton>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  font-size: 0.875rem;
+  color: ${COLORS.gray[300]};
+  background-color: ${COLORS.gray[900]};
+  padding-left: 2rem;
+  padding-right: 2rem;
+  padding-top: 0.75rem;
+  padding-bottom: 0.75rem;
+  display: flex;
+  gap: 2rem;
+  flex-grow: 0;
+`;
+
+const MarketingMessage = styled.span`
+  color: ${COLORS.white};
+  flex-grow: 1;
+`;
+
+const HelpLink = styled.a`
+  color: inherit;
+  text-decoration: none;
+  outline-offset: 2px;
+
+  &:not(:focus-visible) {
+    outline: none;
+  }
+`;
+
+export default SuperHeader;
